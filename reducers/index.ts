1b8c5d219f14b3abaa9145fde906b20c652fbd11
leewooseong <sitpo96@naver.com>
2022-09-21 15:15:04 +0900

    repo 디렉터리 구조 변경

--- reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './user';
-import postReducer from './post';
-
-export default combineReducers({
-    user: userReducer,
-    posts: postReducer,
-});
